f5a0ef742d81f4e8a53ac7115b3912cfb8de925e
Miguel Sanz <118904961+Miguel-Sanz@users.noreply.github.com>
2023-03-08 14:31:51 +0100

    Subida de la segunda parte del código

---
+++ Suite.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package DiagramaClase;
+
+/**
+ * @author David y Miguel
+ *
+ */
+public class Suite extends Habitacion{
+
+	//Atributos de la clase Suite
+	int metros;
+	String servicios;
+	
+	/**
+	 * Constructor de la clase Suite.
+	 * @param numHabitacion Número de la habitación.
+	 * @param estado Estado de la habitación.
+	 * @param numCamas Número de camas de la habitación.
+	 * @param metros Metros de la habitación.
+	 * @param servicios Servicios de la habitación.
+	 */
+	public Suite(int numHabitacion, String estado, int numCamas, int metros, String servicios) {
+		super(numHabitacion, estado, numCamas);
+		this.metros = metros;
+		this.servicios = servicios;
+	}
+	
+	/**
+	 * Obtiene los metros de la Suite.
+	 * @return Metros de la Suite.
+	 */
+	public int getMetros() {
+		return metros;
+	}
+
+	/**
+	 * Asigna los metros de la Suite.
+	 * @param metros Metros de la Suite.
+	 */
+	public void setMetros(int metros) {
+		this.metros = metros;
+	}
+
+	/**
+	 * Obtiene los servicios de la Suite.
+	 * @return Servicios de la Suite.
+	 */
+	public String getServicios() {
+		return servicios;
+	}
+
+	/**
+	 * Asigna los servicios de la Suite.
+	 * @param servicios Servicios de la Suite.
+	 */
+	public void setServicios(String servicios) {
+		this.servicios = servicios;
+	}
+	
+	/**
+	 * Calcula los gastos de la Suite.
+	 */
+	public void calcularGastos() {
+		
+	}
+}
